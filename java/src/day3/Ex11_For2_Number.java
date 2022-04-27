65df2d6be97df85d49068980ed373dc81bbf642a
st8324 <stajun@kut.ac.kr>
2022-04-27 17:35:10 +0900

    자바 3일차 : for문 예제 - 연속된 숫자 출력 예제

---
+++ java/src/day3/Ex11_For2_Number.java
@@ -0,0 +1,21 @@
+package day3;
+
+public class Ex11_For2_Number {
+	
+	public static void main(String[]args) {
+		/* 1부터 5까지 반복문을 이용하여 출력하는 코드를 작성하세요.
+		 * 1	i=1, 1
+		 * 2	i=2, 2
+		 * 3	i=3, 3
+		 * 4	i=4, 4
+		 * 5	i=5, 5
+		 * 		i=n, i를 출력
+		 * 반복횟수 : i는 1부터 5까지 1씩 증가
+		 * 규칙성 : i를 출력
+		 * */
+		int i;
+		for(i=1; i<=5 ; i++) {
+			System.out.println(i);
+		}
+	}
+}
