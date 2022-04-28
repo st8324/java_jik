6cbcb81aabf5acd30f42e37f9a7ba2de79b3f505
st8324 <stajun@kut.ac.kr>
2022-04-28 10:31:16 +0900

    자바 3일차 : for문 예제 - 최대 공약수 예제

--- java/src/day3/Ex15_For6_GCD.java
+++ java/src/day3/Ex15_For6_GCD.java
@@ -15,8 +15,13 @@ public class Ex15_For6_GCD {
 		 * 		  => num1을 i로 나누었을 때 나머지가 0과 같고 num2를 i로 나누었을 때 나머지가 0과 같다면 i를 gcd에 저장
 		 * 반복문 종료 후 : gcd를 출력
 		 * */
-		
-		
+		int num1 = 12, num2= 18, gcd=1;
+		for(int i=1; i <= num1 ; i++ ) {
+			if(num1 % i == 0 && num2 % i == 0) {
+				gcd = i;
+			}
+		}
+		System.out.println(num1 + "과 " + num2 + "의 최대 공약수 : " + gcd);
 		/* 같은 타입의 변수를 여러개 선언하는 경우 ,를 통해 한줄로 선언할 수 있다
 		 * 타입 변수명1;
 		 * 타입 변수명2;
