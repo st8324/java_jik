fb22211d84c53c6ec0bb6680650101931ca40c29
st8324 <stajun@kut.ac.kr>
2022-04-29 17:09:57 +0900

    자바 5일차 : 중첩 반복문 예제 - 별 예제 - 다른 방법 추가

--- java/src/day5/Ex7_NestedLoop2_Star.java
+++ java/src/day5/Ex7_NestedLoop2_Star.java
@@ -42,7 +42,7 @@ public class Ex7_NestedLoop2_Star {
 		}
 		for(int i = 1; i <= num; i++) {
 			for(int j = 1; j<=num; j++) {
-				if(num - i > j ) {
+				if(num - i >= j ) {
 					System.out.print(" ");
 				}else {
 					System.out.print("*");
