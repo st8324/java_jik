b8f73adba3eb6405447e04646db5162a73d68138
st8324 <stajun@kut.ac.kr>
2022-05-06 17:19:39 +0900

    과제 : 리모컨 클래스 생성 예제

---
+++ java/src/day10/Ex1_Class_RemoteController.java
@@ -0,0 +1,14 @@
+package day10;
+
+public class Ex1_Class_RemoteController {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+/* Tv 리모컨 클래스를 생성하고, 객체를 생성 후 기능들을 테스트해보세요. */
+class RemoteController{
+	
+}
