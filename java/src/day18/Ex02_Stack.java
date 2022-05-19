90b8d7319bc9d1f6496721782324889a7c08ef9b
st8324 <stajun@kut.ac.kr>
2022-05-19 17:28:41 +0900

    자바 18일차 : 스택 오버플로우 발생 예제

---
+++ java/src/day18/Ex02_Stack.java
@@ -0,0 +1,13 @@
+package day18;
+
+public class Ex02_Stack {
+
+	public static void main(String[] args) {
+		test();
+	}
+	
+	/* 재귀메소드는 메소드안에서 자기 자신을 호출하는 메소드 */
+	public static void test() {
+		test();
+	}
+}
