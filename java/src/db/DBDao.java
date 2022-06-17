419f4c7038d97fdbf6b003fe8f9224f430672e8e
st8324 <stajun@kut.ac.kr>
2022-06-17 11:45:38 +0900

    DB와 JAVA 연동 예제

--- java/src/db/DBDao.java
+++ java/src/db/DBDao.java
@@ -15,6 +15,8 @@ public class DBDao {
 		this.con = con;
 	}
 	public List<StudentVO> selectStudent(){
+		if(con == null)
+			throw new RuntimeException("DB와 연결되지 않아 실행할 수 없습니다.");
 		String sql = "select * from student";
 		List<StudentVO> list = new ArrayList<StudentVO>();
 		try {
@@ -34,6 +36,8 @@ public class DBDao {
 		return list;
 	}
 	public int insertStudent(String st_num, String st_name, String st_pr_num) {
+		if(con == null)
+			throw new RuntimeException("DB와 연결되지 않아 실행할 수 없습니다.");
 		String sql = "insert into student(st_num, st_name, st_pr_num) values(?,?,?)";
 		try {
 			PreparedStatement ps = con.prepareStatement(sql);
@@ -49,6 +53,8 @@ public class DBDao {
 		}
 	}
 	public int updateStudentName(String st_num, String st_name) {
+		if(con == null)
+			throw new RuntimeException("DB와 연결되지 않아 실행할 수 없습니다.");
 		String sql = "update student set st_name = ? where st_num = ?";
 		try {
 			PreparedStatement ps = con.prepareStatement(sql);
