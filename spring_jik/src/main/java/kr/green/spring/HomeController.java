1bc751b395310863b390d2e3e9de0f8a8725500d
st8324 <stajun@kut.ac.kr>
2022-08-01 14:51:13 +0900

    spring : 데이터 전송 연습 예제

--- spring_jik/src/main/java/kr/green/spring/HomeController.java
+++ spring_jik/src/main/java/kr/green/spring/HomeController.java
@@ -1,15 +1,10 @@
 package kr.green.spring;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -17,23 +12,42 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	/* url에 ?로 데이터를 전송하는 경우
+	 * http://localhost:8080/spring?age=20&hobby=운동&id=abc123&pw=qwe123 
+	 * */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	public ModelAndView home(ModelAndView mv, Integer age, String hobby, String id, String pw) {
+		//mv.addObject("화면에서 사용할 이름", "보낼변수/객체");
+		System.out.println("나이는 "+age+"입니다");
+		System.out.println("취미는 "+hobby+"입니다");
+		System.out.println("아이디는 "+id+"입니다");
+		System.out.println("비번은 "+pw+"입니다");
+		mv.addObject("name", "홍길동");
+		mv.setViewName("home");	
+		return mv;
 	}
-	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public ModelAndView loginGet(ModelAndView mv) {
+		
+		mv.setViewName("member/login");
+		return mv;
+	}
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public ModelAndView loginPost(ModelAndView mv) {
+		
+		mv.setViewName("member/login");
+		return mv;
+	}
+	/* url 경로에 전달하려는 값이 있는 경우 
+	 * http://localhost:8080/spring/member/20/운동 
+	 * */
+	@RequestMapping(value="/member/{age1}/{hobby1}", method=RequestMethod.GET)
+	public ModelAndView ageHobbyGet(ModelAndView mv,
+			@PathVariable("age1") int age, 
+			@PathVariable("hobby1") String hobby) {
+		System.out.println("나이는 "+age+"입니다");
+		System.out.println("취미는 "+hobby+"입니다");
+		mv.setViewName("home");
+		return mv;
+	}
 }
--- spring_jik/src/main/webapp/WEB-INF/views/home.jsp
+++ spring_jik/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,5 +1,6 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
 <html>
 <head>
 	<title>Home</title>
@@ -9,6 +10,14 @@
 	Hello world!  
 </h1>
 
-<P>  The time on the server is ${serverTime}. </P>
+<P> 안녕하세요. 제 이름은 ${name} 입니다. </P>
+<a href="/spring?age=20">나이는 20살입니다.</a>
+<form action="/spring" method="get">
+	<input type="text" name="hobby"> <br>
+	<input type="text" name="id" placeholder="아이디"> <br>
+	<input type="password" name="pw" placeholder="비번"> <br>
+	<button>전송</button>
+</form>
+<a href="/spring/login">로그인 페이지로</a>
 </body>
 </html>
