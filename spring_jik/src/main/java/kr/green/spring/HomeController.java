90a0edd3e519f23b20495dc90c7e22ed5669a43f
st8324 <stajun@kut.ac.kr>
2022-08-02 12:03:19 +0900

    spring : 타일즈 최적화 완료

--- spring_jik/src/main/java/kr/green/spring/HomeController.java
+++ spring_jik/src/main/java/kr/green/spring/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 	
-	@RequestMapping(value= {"/main/home","/home.do"})
+	@RequestMapping(value="/")
 	public ModelAndView openTilesView(ModelAndView mv) throws Exception{
 	    mv.setViewName("/main/home");
 	    mv.addObject("setHeader", "타일즈");
--- spring_jik/src/main/webapp/WEB-INF/views/layout/baseLayout.jsp
+++ spring_jik/src/main/webapp/WEB-INF/views/layout/baseLayout.jsp
@@ -2,7 +2,7 @@
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="tiles" uri="http://tiles.apache.org/tags-tiles"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
- 
+<!DOCTYPE html>
 <html>
 <head>
 <title>스프링</title>
